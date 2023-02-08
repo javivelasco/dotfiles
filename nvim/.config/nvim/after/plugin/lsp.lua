0701449af118aade2bacb5c37a6f71d178cd4afe
Javi Velasco <javier.velasco86@gmail.com>
2023-02-08 10:55:56 +0100

    Configure `null-ls` for `eslint` and formatting

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -59,6 +59,10 @@ lsp.on_attach(function(client, bufnr)
     vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
     vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
     vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format() end, opts)
+    if client.name == "tsserver" then
+        client.server_capabilities.documentFormattingProvider = false
+    end
 end)
 
 lsp.setup()
