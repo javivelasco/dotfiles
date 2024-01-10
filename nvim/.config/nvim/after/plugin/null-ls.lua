a69d3717826e07c1c47ccc7a9872201e5c0c05d2
Javi Velasco <javier.velasco86@gmail.com>
2024-01-10 11:00:49 +0100

    Add `eslint`

--- nvim/.config/nvim/after/plugin/null-ls.lua
+++ nvim/.config/nvim/after/plugin/null-ls.lua
@@ -3,16 +3,12 @@ if not null_ls_status_ok then
 	return
 end
 
-local formatting = null_ls.builtins.formatting
--- local diagnostics = null_ls.builtins.diagnostics
--- local code_actions = null_ls.builtins.code_actions
-
 null_ls.setup({
 	debug = false,
 	sources = {
-		formatting.prettierd,
-		formatting.stylua,
-		-- diagnostics.eslint,
-		-- code_actions.eslint
+		null_ls.builtins.formatting.prettierd,
+		null_ls.builtins.formatting.stylua,
+    null_ls.builtins.diagnostics.eslint_d,
+    null_ls.builtins.code_actions.eslint_d
 	},
 })
