e8168f0f2824f019fdb5570f3611faaf566544e6
Javi Velasco <javier.velasco86@gmail.com>
2024-04-19 00:28:39 +0200

    Update and update treesitter config

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,104 +1,49 @@
 return {
   {
-    -- Highlight, edit, and navigate code
     "nvim-treesitter/nvim-treesitter",
     dependencies = {
-      { "nvim-treesitter/nvim-treesitter-textobjects" },
       { "JoosepAlviste/nvim-ts-context-commentstring", opts = {} },
       { "nvim-treesitter/nvim-treesitter-context", opts = { max_lines = 3, multiline_threshold = 1 } },
     },
-    build = ":TSUpdate",
+    build = function()
+      vim.cmd(":TSUpdate")
+    end,
     config = function()
       ---@diagnostic disable-next-line: missing-fields
       require("nvim-treesitter.configs").setup({
-        -- A list of parser names, or "all"
         ensure_installed = {
+          "typescript",
           "vimdoc",
           "javascript",
-          "typescript",
-          "c",
-          "cpp",
-          "python",
-          "vim",
-          "tsx",
-          "go",
-          "lua",
           "rust",
+          "lua",
+          "go",
+          "tsx",
+          "vim",
+          "python",
+          "toml",
+          "prisma",
         },
-
-        -- Install parsers synchronously (only applied to `ensure_installed`)
-        sync_install = false,
-
-        -- Automatically install missing parsers when entering buffer
-        -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-        auto_install = true,
-
+        ignore_install = { "phpdoc" }, -- List of parsers to ignore installing
         highlight = {
-          -- `false` will disable the whole extension
-          enable = true,
-
+          enable = true, -- false will disable the whole extension
+          disable = { "c", "rust", "lua" }, -- list of language that will be disabled
           -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
           -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
           -- Using this option may slow down your editor, and you may see some duplicate highlights.
           -- Instead of true it can also be a list of languages
           additional_vim_regex_highlighting = false,
         },
-
-        indent = { enable = true, disable = { "python" } },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = "<c-space>",
-            node_incremental = "<c-space>",
-            scope_incremental = "<c-s>",
-            node_decremental = "<M-space>",
-          },
-        },
-        textobjects = {
-          select = {
-            enable = true,
-            lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-            keymaps = {
-              -- You can use the capture groups defined in textobjects.scm
-              ["aa"] = "@parameter.outer",
-              ["ia"] = "@parameter.inner",
-              ["af"] = "@function.outer",
-              ["if"] = "@function.inner",
-              ["ac"] = "@class.outer",
-              ["ic"] = "@class.inner",
-            },
-          },
-          move = {
-            enable = true,
-            set_jumps = true, -- whether to set jumps in the jumplist
-            goto_next_start = {
-              ["]m"] = "@function.outer",
-              ["]]"] = "@class.outer",
-            },
-            goto_next_end = {
-              ["]M"] = "@function.outer",
-              ["]["] = "@class.outer",
-            },
-            goto_previous_start = {
-              ["[m"] = "@function.outer",
-              ["[["] = "@class.outer",
-            },
-            goto_previous_end = {
-              ["[M"] = "@function.outer",
-              ["[]"] = "@class.outer",
-            },
-          },
-          swap = {
-            enable = true,
-            swap_next = {
-              ["<leader>a"] = "@parameter.inner",
-            },
-            swap_previous = {
-              ["<leader>A"] = "@parameter.inner",
-            },
-          },
-        },
       })
+
+      local parser_configs = require("nvim-treesitter.parsers").get_parser_configs()
+      parser_configs.http = {
+        install_info = {
+          url = "https://github.com/NTBBloodbath/tree-sitter-http",
+          files = { "src/parser.c" },
+          branch = "main",
+        },
+      }
     end,
   },
 
