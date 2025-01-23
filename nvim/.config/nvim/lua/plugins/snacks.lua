a4c6a3160ba73cf107c053ea047e563ab1ad660c
Javi Velasco <javier.velasco86@gmail.com>
2025-01-23 10:23:04 +0100

    Add dashboard config

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -5,7 +5,31 @@ return {
   lazy = false,
   opts = {
     bigfile = { enabled = true },
-    dashboard = { enabled = true },
+    dashboard = {
+      enabled = true,
+      preset = {
+        keys = {
+          { key = 's', icon = ' ', desc = 'Load session (directory)', action = ':lua require("persistence").load()' },
+          { key = 'l', icon = ' ', desc = 'Load last session', action = ':lua require("persistence").load({ last = true })' },
+          { key = 'f', icon = ' ', desc = 'Find files', action = ':lua require("telescope.builtin").git_files({ silent = true })' },
+          { key = 'r', icon = ' ', desc = 'Search in files', action = ':lua require("telescope.builtin").live_grep({ silent = true })' },
+        },
+        header = '',
+      },
+      sections = {
+        {
+          section = 'terminal',
+          cmd = [[
+set WALLPAPER $(echo "$HOME/.config/nvim/lua/background/golden.jpg" | shuf -n 1) && chafa "$WALLPAPER" --format symbols --symbols vhalf --size 60x14 --stretch
+            ]],
+          height = 16,
+          padding = 3,
+        },
+        { section = 'keys',   gap = 1, padding = 3 },
+        { section = 'startup' },
+      },
+
+    },
     notifier = {
       enabled = true,
       timeout = 3000,
