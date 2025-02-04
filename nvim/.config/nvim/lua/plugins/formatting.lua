5bdcf5d26d6b469815fd564782ddb0e0e572aceb
Javi Velasco <javier.velasco86@gmail.com>
2025-02-04 10:02:20 +0100

    Update formaters

--- nvim/.config/nvim/lua/plugins/formatting.lua
+++ nvim/.config/nvim/lua/plugins/formatting.lua
@@ -24,11 +24,6 @@ return {
       },
     },
     opts = {
-      formatters = {
-        biome = {
-          require_cwd = true,
-        },
-      },
       formatters_by_ft = {
         css = { "prettierd" },
         html = { "prettierd" },
@@ -42,11 +37,23 @@ return {
         typescriptreact = biome_or_prettierd,
         yaml = { "prettierd" },
       },
-      format_on_save = {
-        lsp_fallback = true,
-        async = false,
+      -- Set default options
+      default_format_opts = {
+        lsp_format = "fallback",
       },
-    }
+      -- Set up format-on-save
+      format_on_save = { timeout_ms = 500 },
+      -- Customize formatters
+      formatters = {
+        shfmt = {
+          prepend_args = { "-i", "2" },
+        },
+      },
+    },
+    init = function()
+      -- If you want the formatexpr, here is the place to set it
+      vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+    end,
   },
 
   {
