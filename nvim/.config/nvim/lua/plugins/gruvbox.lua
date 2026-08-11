1adb582e24cf944f09ff6b2d558dcda5f549d1e7
Javi Velasco <javier.velasco86@gmail.com>
2026-08-12 00:58:44 +0200

    Better color

--- nvim/.config/nvim/lua/plugins/gruvbox.lua
+++ nvim/.config/nvim/lua/plugins/gruvbox.lua
@@ -4,6 +4,14 @@ return {
   priority = 1000,
   branch = "main",
   config = function()
+    require("gruvbox").setup({
+      overrides = {
+        -- Blue folders in neo-tree instead of gruvbox's green-bold Directory
+        -- (upstream d7a1674 made them green to match oil.nvim)
+        NeoTreeDirectoryIcon = { link = "GruvboxBlue" },
+        NeoTreeDirectoryName = { link = "GruvboxBlueBold" },
+      },
+    })
     vim.cmd("colorscheme gruvbox")
   end,
 }
