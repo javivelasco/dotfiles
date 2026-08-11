9944cecb0352a085b6cb9a1d6c704515b9d10042
Javi Velasco <javier.velasco86@gmail.com>
2026-08-12 00:49:02 +0200

    Updates

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -241,3 +241,7 @@ fish_add_path "/Users/javivelasco/.local/share/fnm/node-versions/v24.18.0/instal
 
 # opencode
 fish_add_path /Users/javivelasco/.opencode/bin
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
